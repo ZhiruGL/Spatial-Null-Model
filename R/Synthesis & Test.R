4ecc6a6240d6ef1264acb6b06fa4a68381a246ff
Zhiru Guoli <zhiruguoli@LSEEB-KFXGFJ2PY4.local>
2025-10-30 12:56:59 -0700

    This is for me to test the function

---
+++ R/Synthesis & Test.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+# Create a 3x3 interaction matrix
+set.seed(123)  # optional, for reproducibility
+
+dcf <- matrix(runif(9, min =-10, max = 10), nrow = 3)
+
+# Assign row and column names
+rownames(dcf) <- c("dog", "cat", "fly")
+colnames(dcf) <- c("dog", "cat", "fly")
+
+# Print the matrix
+dcf
+
+result=simulate_point_process(spatial_association=dcf,
+                              gamma=3,
+                              radius=0.1,
+                              num_candidates = 1000,
+                              Npoints = 50,
+                              count_max = 10,
+                              names=TRUE)
+print(result)
+ggplot(result, aes(x = x, y = y, color = species)) +
+  geom_point(size = 3, alpha = 0.8) +
+  theme_minimal() +
+  labs(
+    title = "Simulated Spatial Pattern by Species",
+    x = "X coordinate",
+    y = "Y coordinate"
+  ) +
+  coord_equal()
+
